7a19a84f55bf613549788b17d672c996b58b4d5b
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2024-02-22 21:21:53 +0100

    kern: Add string.h standard functionality.
    Add some basic functions for memory manipulation as defined by the C
    standard library.
    
    * kern/CMakeLists.txt (kern): Add string.c as a source file.
    * kern/string.c: New file.
    * kern/string.h: New file.

---
+++ kern/string.h
@@ -0,0 +1,12 @@
+#ifndef BRADYPOS_KERN_STRING_H
+#define BRADYPOS_KERN_STRING_H
+
+#include <stddef.h>
+
+size_t strlen(const char *str);
+void *memset(void *dest, int ch, size_t size);
+int memcmp(const void *lhs, const void *rhs, size_t count);
+void *memcpy(void *dest, const void *src, size_t size);
+void *memmove(void *dest, const void *src, size_t size);
+
+#endif
