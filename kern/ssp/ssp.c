067df6696b35a01b80611ddd5f78f51e48c0222b
Tim Gesthuizen <tim.gesthuizen@yahoo.de>
2024-02-24 16:45:54 +0100

    kern: Add support for SSP.
    The kernel can now be built with stack smash protection.
    It is by default enabled in the Debug build configuration.
    Stack smash detection makes the compiler place stack canaries in
    suspicous functions. At the end of the function the compiler will
    check whether the canary has been overwritten. If this is the case it
    will call __stack_chk_fail, allowing common memory handling errors on
    the stack to be caught.
    
    * kern/CMakeLists.txt: Add ssp subdirectory.
    * kern/ssp: New kernel module.

---
+++ kern/ssp/ssp.c
@@ -0,0 +1,8 @@
+#include <kern/debug.h>
+#include <stdint.h>
+
+const uintptr_t __stack_chk_guard = 0xe2dee396;
+
+__attribute__((noreturn)) void __stack_chk_fail(void) {
+  panic("Smashed stack detected!");
+}
