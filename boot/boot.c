7b2ed7c7ca7ba5ae1a2e59f1b985621b4d2f00db
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-18 14:50:49 +0200

    boot: Link a functional boot loader.
    Link the boot2 bootloader and set a vector table after it so that the
    actual boot loader gets fired up correctly.
    
    * boot/CMakeLists.txt: Rework linking of boot executable and add
      bs2_default_padded_checksummed.S to executable.
    * boot/boot.c: Add a vector table to start from.
    * boot/bootloader.ld: New linker script for bootloader.

--- boot/boot.c
+++ boot/boot.c
@@ -1,4 +1,44 @@
+#include <stdint.h>
+
+extern unsigned char __stack[];
 static int main();
+void _start();
+static __attribute__((naked)) void isr_invalid() {
+  __asm__ volatile("bkpt #0\n");
+}
+static __attribute__((naked)) void unhandled_irq() {
+  __asm__ volatile("mrs r0, ipsr\n\t"
+                   "sub r0, #16\n\t"
+                   ".global unhandled_user_irq_num_in_r0\n"
+                   "unhandled_user_irq_num_in_r0:\n\t"
+                   "bkpt #0\n\t");
+}
+
+__attribute__((section(".vector"))) const uintptr_t __vector[] = {
+    (uintptr_t)__stack,       (uintptr_t)_start,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)isr_invalid,   (uintptr_t)isr_invalid,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq,
+    (uintptr_t)unhandled_irq, (uintptr_t)unhandled_irq};
 
 __attribute__((naked)) void _start() {
   asm volatile("movs r0, #0\n\t"
