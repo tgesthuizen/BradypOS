a3eb77c1eb5708338157f5c7616b5752ef339aa0
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-19 16:41:50 +0200

    kern: Compile image for flash storage.
    * kern/platform/rp2040/CMakeLists.txt: Add necessary flags for
      position independent executable linking, and link for flash storage.
    * kern/platform/rp2040/pico_flash.ld: New file.
    * kern/platform/rp2040/start.s: Reference stack through GOT.

--- kern/platform/rp2040/start.s
+++ kern/platform/rp2040/start.s
@@ -7,16 +7,21 @@
 	.thumb_func
 _start:
 	cpsid i
-	movs r0, #0
-	mov lr, r0
-	ldr r0, =__stack
-	msr msp, r0
-	msr psp, r0
-	bl main
+	movs  r0, #0
+	mov   lr, r0
+	ldr   r0, .Lstack_got_offset
+	mov   r1, r9
+	ldr   r0, [r1, r0]
+	msr   msp, r0
+	msr   psp, r0
+	bl    main
 	
 busy_loop:
 	wfi
-	b busy_loop
+	b   busy_loop
 
+	.align 2
+.Lstack_got_offset:
+	.word __stack(GOT)
 	.pool
 	.endfunc
