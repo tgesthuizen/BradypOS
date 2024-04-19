0e23cc8453abdcaf80759f7af665d089885a5b06
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-19 14:45:11 +0200

    boot: Add code for loading the core files.
    The bootloader now locates the kernel and root binaries. Next step is
    to load the ELF binaries.
    
    * boot/CMakeLists.txt: Link libromfs into boot.
    * boot/boot.c: Add code traversing the ROMFS to load `/sbin/kern` and
      `/bin/root`.

--- boot/boot.c
+++ boot/boot.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 extern unsigned char __stack[];
-static int main();
+int main();
 void _start();
 static __attribute__((naked)) void isr_invalid() {
   __asm__ volatile("bkpt #0\n");
@@ -75,6 +75,61 @@ __attribute__((naked)) void _start() {
                    : "r0", "r1", "r2", "lr");
 }
 
-extern void *romfs_start;
+extern unsigned char *__romfs_start;
 
-static int main() {}
+static int romfs_rom_map(void **data, size_t offset, size_t size, void *user) {
+  (void)size;
+  (void)user;
+  *data = __romfs_start + offset;
+  return 0;
+}
+
+static void romfs_rom_unmap(void **data, size_t size, void *user) {
+  (void)data;
+  (void)size;
+  (void)user;
+}
+
+static const struct romfs_block_iface romfs_rom_mapping = {
+    .map = romfs_rom_map, .unmap = romfs_rom_unmap};
+
+static bool memcmp(const char *lhs, const char *rhs, size_t len) {
+  while (len--) {
+    if (*lhs++ != *rhs++)
+      return false;
+  }
+  return true;
+}
+
+int main() {
+  struct romfs_info info_block;
+  if (!romfs_info(&romfs_rom_mapping, NULL, &info_block)) {
+    // TODO: Use a panic function here
+    return 1;
+  }
+
+  static const char sbin_dir_name[] = "sbin";
+  const size_t first_file = romfs_root_directory(&romfs_rom_mapping, NULL);
+  struct romfs_file_info file_info;
+  size_t sbin_dir = ROMFS_INVALID_FILE;
+  for (size_t current_file = first_file; current_file != ROMFS_INVALID_FILE;
+       current_file = romfs_next_file(&romfs_rom_mapping, current_file, NULL)) {
+    if (!romfs_file_info(&romfs_rom_mapping, current_file, &file_info, NULL))
+      continue;
+    // This is safe: The buffer for the filename is at least 16 bytes long
+    if (memcmp(file_info.name, sbin_dir_name, sizeof(sbin_dir_name))) {
+      sbin_dir = current_file;
+      break;
+    }
+  }
+  size_t kern_file = romfs_openat(&romfs_rom_mapping, sbin_dir, "kern", NULL);
+  size_t root_file = romfs_openat(&romfs_rom_mapping, sbin_dir, "root", NULL);
+  const unsigned char *const kern_elf_base =
+      __romfs_start +
+      romfs_file_content_offset(&romfs_rom_mapping, kern_file, NULL);
+  const unsigned char *const root_elf_base =
+      __romfs_start +
+      romfs_file_content_offset(&romfs_rom_mapping, root_file, NULL);
+
+  return 0;
+}
