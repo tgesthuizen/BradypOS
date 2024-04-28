6521bac00f1347f03e7039e86a66226421cf2f69
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-28 10:42:10 +0200

    kern: Simplify stack literal loading.
    No need to place the stack variable in the GOT.
    
    * kern/platform/rp2040/start.s (_start): Refer directly to stack
      address.

--- kern/platform/rp2040/start.s
+++ kern/platform/rp2040/start.s
@@ -9,7 +9,7 @@ _start:
 	cpsid i
 	movs  r0, #0
 	mov   lr, r0
-	ldr   r0, .Lstack_loc
+	ldr   r0, =0x20040000
 	msr   msp, r0
 	msr   psp, r0
 	bl    main
@@ -18,8 +18,5 @@ busy_loop:
 	wfi
 	b   busy_loop
 
-	.align 2
-.Lstack_loc:
-	.word 0x20040000
 	.pool
 	.endfunc
