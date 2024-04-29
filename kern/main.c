c4b125294e44f8268030304f5ea2d328c22a5d57
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-29 11:49:18 +0200

    kern: Initialize systick and NVIC and reset after kernel finishes.
    * kern/main.c (main): Initialize NVIC and Systick, set reset to
      pending before exit.

--- kern/main.c
+++ kern/main.c
@@ -1,5 +1,7 @@
 #include <kern/debug.h>
+#include <kern/nvic.h>
 #include <kern/platform.h>
+#include <kern/systick.h>
 #include <stdint.h>
 
 int main()
@@ -7,7 +9,13 @@ int main()
     // Interrupts should be disabled. But better be safe than sorry.
     disable_interrupts();
     *(volatile uintptr_t *)(PPB_BASE + VTOR_OFFSET) = (uintptr_t)__vector;
+    systick_init();
+    nvic_init();
     enable_interrupts();
 
     dbg_puts("Interrupts are enabled, we have booted!\n");
+
+    dbg_puts("Resetting...");
+    nvic_set_pending(EXC_RESET, true);
+    return 0;
 }
