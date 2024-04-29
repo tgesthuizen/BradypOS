633b9686e3b9b1101088485114f2e52fe4daa8b7
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-29 09:22:36 +0200

    kern: Remove LED toggling code.
    * kern/main.c (toggle_led): Deleted.
      (main): Remove GPIO setup.

--- kern/main.c
+++ kern/main.c
@@ -4,18 +4,10 @@
 
 int main()
 {
+    // Interrupts should be disabled. But better be safe than sorry.
+    disable_interrupts();
     *(volatile uintptr_t *)(PPB_BASE + VTOR_OFFSET) = (uintptr_t)__vector;
     enable_interrupts();
 
     dbg_puts("Interrupts are enabled, we have booted!\n");
-
-    volatile uint32_t *const target_gpio =
-        (volatile uint32_t *)(IO_BANK0_BASE + GPIO_BLOCK_SIZE * TARGET_GPIO);
-    target_gpio[1] = PIN_FUNC_SIO | 3 << 12;
-    *(volatile uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET) = 1 << 25;
-}
-
- __attribute__((visibility("default"))) void toggle_led()
-{
-    *(volatile uint32_t *)(SIO_BASE + SIO_GPIO_OUT_XOR) = 1 << 25;
 }
