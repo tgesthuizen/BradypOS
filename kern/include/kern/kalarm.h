053a6cf0bbb8a47c23a207ee1f301ac37e37f746
Tim Gesthuizen <tim@gesthuizen.net>
2024-04-29 16:04:57 +0200

    kern: Add rough sketch of a kalarm system.
    Add a system that will call handlers at appropriate times. This can be
    used to implement preemptive multitasking, timeouts and other timed
    events in the kernel.
    It uses a min-heap as a data structure to reduce the managing time.
    
    * kern/CMakeLists.txt: Compile kalarm.c into kernel.
    * kern/kalarm.c: New file.
    * kern/include/kern/kalarm.h: New file.

---
+++ kern/include/kern/kalarm.h
@@ -0,0 +1,7 @@
+#ifndef BRADYPOS_KERN_KALARM_H
+#define BRADYPOS_KERN_KALARM_H
+
+void kalarm_init();
+void register_kalarm_event(unsigned long when, void (*what)());
+
+#endif
---
+++ kern/kalarm.c
@@ -0,0 +1,78 @@
+#include <kern/debug.h>
+#include <kern/kalarm.h>
+#include <kern/systick.h>
+#include <stdbool.h>
+
+struct kalarm_event
+{
+    unsigned long when;
+    void (*what)();
+};
+
+enum
+{
+    MAX_KALARM_EVENTS = 16
+};
+static struct kalarm_event kalarm_events[MAX_KALARM_EVENTS];
+static unsigned kalarm_count;
+static unsigned long current_time;
+
+void kalarm_init() { kalarm_count = 0; }
+
+static void swap_kalarm_event(struct kalarm_event *lhs,
+                              struct kalarm_event *rhs)
+{
+    const struct kalarm_event tmp = *lhs;
+    *lhs = *rhs;
+    *rhs = tmp;
+}
+
+void register_kalarm_event(unsigned long when, void (*what)())
+{
+    if (kalarm_count == MAX_KALARM_EVENTS)
+    {
+        panic("Kernel is out of kalarms!\n");
+    }
+    // Add event
+    unsigned idx = kalarm_count++;
+    kalarm_events[idx] = (struct kalarm_event){.when = when, .what = what};
+    // And heapify
+    for (unsigned parent = idx / 2;
+         idx && (kalarm_events[idx].when < kalarm_events[parent].when);
+         idx = parent, parent = (idx - 1) / 2)
+    {
+        swap_kalarm_event(&kalarm_events[idx], &kalarm_events[parent]);
+    }
+}
+
+static bool has_pending_kalarm()
+{
+    return kalarm_count && kalarm_events[0].when <= current_time;
+}
+static struct kalarm_event pop_kalarm_event()
+{
+    const struct kalarm_event result = kalarm_events[0];
+    unsigned idx = 0;
+    const unsigned leaf_node_idx = kalarm_count / 2;
+    while (idx < leaf_node_idx)
+    {
+        const unsigned idx_left = idx * 2 + 1;
+        const unsigned idx_right = idx * 2 + 2;
+        const unsigned target_idx =
+            (kalarm_events[idx_left].when <= kalarm_events[idx_right].when)
+                ? idx_left
+                : idx_right;
+        kalarm_events[idx] = kalarm_events[target_idx];
+        idx = target_idx;
+    }
+    return result;
+}
+
+void isr_systick()
+{
+    ++current_time;
+    while (has_pending_kalarm())
+    {
+        pop_kalarm_event().what();
+    }
+}
