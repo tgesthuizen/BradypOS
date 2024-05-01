524f977b6cc86123d50839c06803de7f4c3e0ef0
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-01 23:24:59 +0200

    kern: Add sketch for thread system.
    * kern/thread.c: New file.
    * kern/include/kern/thread.h: New file.
    * kern/CMakeLists.txt: Build thread.c.

---
+++ kern/include/kern/thread.h
@@ -0,0 +1,46 @@
+#ifndef BRADYPOS_KERN_TASK_H
+#define BRADYPOS_KERN_TASK_H
+
+#include "types.h"
+#include <l4/utcb.h>
+
+enum thread_kind_t
+{
+    TK_KERNEL,
+    TK_IDLE,
+    TK_ROOT,
+    TK_INTERRUPT,
+    TK_USER,
+};
+
+enum thread_state_t
+{
+    TS_INACTIVE,
+    TS_RUNNABLE,
+    TS_SVC_BLOCKED,
+    TS_RECV_BLOCKED,
+    TS_SEND_BLOCKED,
+};
+
+struct thread_context_t
+{
+    unsigned r[16];
+    unsigned ctl;
+};
+
+struct as_t;
+
+struct tcb_t
+{
+    L4_thread_t global_id;
+    L4_thread_t local_id;
+    enum thread_state_t state;
+    struct thread_context_t ctx;
+    struct as_t *as;
+    struct utcb_t *utcb;
+
+    L4_thread_t ipc_from;
+    uint32_t timeout_event;
+};
+
+#endif
