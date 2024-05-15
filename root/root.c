9aef413935064a3278876dedc60e4361377379bd
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-15 16:18:54 +0200

    root: Store starting time.
    This change is much more about testing the syscall than adding
    functionality.
    
    * root/root.c (starting_time): New variable.
      (main): Store starting_time.

--- root/root.c
+++ root/root.c
@@ -5,10 +5,12 @@
 __attribute__((naked)) void _start() { __asm__("b main\n\t"); }
 
 static kip_t *the_kip;
+static L4_clock_t starting_time;
 
 int main()
 {
     the_kip = L4_kernel_interface(NULL, NULL, NULL);
+    starting_time = L4_system_clock();
 
     while (1)
         L4_yield();
