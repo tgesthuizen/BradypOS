0f672c30db1120916759242c5d170ae08919fccc
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-18 14:08:38 +0200

    l4: Add one more error code.
    * include/l4/errors.h (enum L4_error_code): Add
      L4_error_invalid_parameter.

--- include/l4/errors.h
+++ include/l4/errors.h
@@ -7,6 +7,7 @@ enum L4_error_code
     L4_error_invalid_thread = 2,
     L4_error_invalid_space = 3,
     L4_error_invalid_scheduler = 4,
+    L4_error_invalid_parameter = 5,
     L4_error_utcb_area = 6,
     L4_error_kip_area = 7,
     L4_error_no_memory = 8,
