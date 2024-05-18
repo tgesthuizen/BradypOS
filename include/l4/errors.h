f925b955fded113b2b177a98cfaf5f15a0f9e8d6
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-18 12:12:49 +0200

    l4: Add errors.h header.
    The header contains an enum of error codes that L4 system calls can
    return.
    
    * include/l4/errors.h: New file.

---
+++ include/l4/errors.h
@@ -0,0 +1,17 @@
+#ifndef BRADYPOS_L4_ERRORS_H
+#define BRADYPOS_L4_ERRORS_H
+
+enum L4_error_code
+{
+    L4_error_no_privilege = 1,
+    L4_error_invalid_thread = 2,
+    L4_error_invalid_space = 3,
+    L4_error_invalid_scheduler = 4,
+    L4_error_utcb_area = 6,
+    L4_error_kip_area = 7,
+    L4_error_no_memory = 8,
+};
+
+typedef enum L4_error_code L4_error_code_t;
+
+#endif
