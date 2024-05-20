73514471caeae488cdb264099476d7ea81f30c1a
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-20 19:39:53 +0200

    root: More work on creating the romfs server.
    Add some more IPC calls that are required to start running the romfs
    server. However, the kernel doesn't serve them properly yet, so wait
    until kernel development has catched up.
    
    * root/root.c (main): Create romfs thread and send pager start
      message.

--- root/root.c
+++ root/root.c
@@ -20,32 +20,55 @@ static void kill_root_thread()
                           L4_NILTHREAD, (void *)(unsigned)-1);
 }
 
+enum
+{
+    UTCB_ALIGN = 160,
+    ROMFS_SERVER_STACK_SIZE = 256
+};
+static unsigned char romfs_server_stack[ROMFS_SERVER_STACK_SIZE];
+
 int main()
 {
 
     the_kip = L4_kernel_interface(NULL, NULL, NULL);
     unsigned old_ctrl;
-    my_thread_id = L4_global_id(46, 1);
+    my_thread_id = L4_global_id(0x40, 1);
 
     starting_time = L4_system_clock();
 
     // The kernel has cheated a bit when it created us.
     // Add the necessary pages and address space information now in the
     // aftermath.
-    if (L4_space_control(
-            L4_my_global_id(), 0, L4_fpage_log2((unsigned)the_kip, 10),
-            L4_fpage_log2((unsigned)&__utcb, 9), L4_NILTHREAD, &old_ctrl) != 1)
+    if (L4_space_control(my_thread_id, 0, L4_fpage_log2((unsigned)the_kip, 10),
+                         L4_fpage_log2((unsigned)&__utcb, 9), L4_NILTHREAD,
+                         &old_ctrl) != 1)
     {
         kill_root_thread();
     }
 
     const L4_thread_id romfs_thread_id = L4_global_id(47, 1);
     if (L4_thread_control(romfs_thread_id, my_thread_id, L4_NILTHREAD,
-                          my_thread_id, (unsigned char *)&__utcb + 160) != 0)
+                          my_thread_id,
+                          (unsigned char *)&__utcb + UTCB_ALIGN) != 0)
     {
         kill_root_thread();
     }
 
+    unsigned *romfs_sp =
+        (unsigned *)(romfs_server_stack + ROMFS_SERVER_STACK_SIZE) - 1;
+    *romfs_sp = (unsigned)((unsigned char *)&__utcb + UTCB_ALIGN);
+
+    extern void romfs_start();
+    L4_set_msg_tag((L4_msg_tag_t){.u = 2, .t = 0, .flags = 0, .label = 0});
+    L4_load_mr(1, (unsigned)&romfs_start);
+    L4_load_mr(2, (unsigned)romfs_sp);
+    L4_thread_id real_from;
+    const L4_msg_tag_t answer_tag =
+        L4_ipc(romfs_thread_id, my_thread_id, 0, &real_from);
+    if (answer_tag.flags & (1 << 3))
+    {
+        kill_root_thread();
+    }
     while (1)
         L4_yield();
 }
