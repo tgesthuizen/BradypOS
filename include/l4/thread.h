6c0068548a083e115311790ddd81d0003d0ddb76
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-21 09:35:47 +0200

    l4: Fix incorrect syscall constant.
    L4_thread_control was issuing a completely broken space control
    syscall, because the constant was incorrect.
    
    * include/l4/thread.h (L4_thread_control): Use SYS_THREAD_CONTROL
      instead of SYS_SPACE_CONTROL.

--- include/l4/thread.h
+++ include/l4/thread.h
@@ -45,10 +45,10 @@ inline unsigned L4_thread_control(L4_thread_id dest,
     register L4_thread_id rp asm("r3") = pager;
     register void *rul asm("r4") = utcb_location;
     unsigned ret;
-    asm volatile("movs r7, %[SYS_SPACE_CONTROL]\n\t"
+    asm volatile("movs r7, %[SYS_THREAD_CONTROL]\n\t"
                  "svc #0\n\t"
                  : "=r"(ret)
-                 : [SYS_SPACE_CONTROL] "i"(SYS_SPACE_CONTROL), "0"(rdest),
+                 : [SYS_THREAD_CONTROL] "i"(SYS_THREAD_CONTROL), "0"(rdest),
                    "r"(rss), "r"(rs), "r"(rp), "r"(rul)
                  : "r7");
     return ret;
