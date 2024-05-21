d39b9cabc3e64efc2fecbde1f08da7016dfafffd
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-21 16:27:39 +0200

    kern: Initialize new address space in SpaceControl if required.
    * kern/memory.c (MAX_ADDRESS_SPACES): New constant.
      (as_pool): New data structure.
      (as_allocated): New variable.
      (alloc_as): New function.
      (create_as): New function.
      (syscall_space_control): Add address space to thread if missing.

--- kern/memory.c
+++ kern/memory.c
@@ -1,3 +1,4 @@
+#include <kern/debug.h>
 #include <kern/memory.h>
 #include <kern/thread.h>
 #include <l4/errors.h>
@@ -9,11 +10,48 @@
 extern kip_t the_kip;
 static L4_fpage_t kip_fpage;
 
+enum
+{
+    MAX_ADDRESS_SPACES = 16
+};
+
+static struct as_t as_pool[MAX_ADDRESS_SPACES];
+static unsigned as_allocated;
+
 void init_memory()
 {
     kip_fpage.b = (unsigned)&the_kip / 512;
     kip_fpage.s = 10;
     kip_fpage.perm = L4_readable;
+    as_allocated = 0;
+}
+
+static unsigned alloc_as()
+{
+    unsigned i;
+    for (i = 0; i < MAX_ADDRESS_SPACES; ++i)
+    {
+        if (!(as_allocated & (1 << i)))
+        {
+            as_allocated |= (1 << i);
+            break;
+        }
+    }
+    return i;
+}
+
+struct as_t *create_as(struct tcb_t *owner)
+{
+    unsigned as_idx = alloc_as();
+    if (as_idx == MAX_ADDRESS_SPACES)
+    {
+        panic("Could not allocate new address space");
+    }
+    struct as_t *result = &as_pool[as_idx];
+    result->first_thread = owner;
+    result->utcb_page = L4_nilpage;
+    result->ref_count = 1;
+    return result;
 }
 
 extern struct tcb_t *caller;
@@ -50,6 +88,15 @@ void syscall_space_control()
         sp[THREAD_CTX_STACK_R0] = 0;
         caller->utcb->error = L4_error_utcb_area;
         return;
+    }
+
+    if (target_thread->as == NULL)
+    {
+        struct as_t *new_as = create_as(target_thread);
+        target_thread->next_sibling = target_thread;
+        target_thread->prev_sibling = target_thread;
+        new_as->utcb_page = L4_nilpage;
+        target_thread->as = new_as;
     }
 
     if (target_thread->as->utcb_page.raw != 0)
