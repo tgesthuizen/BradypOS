273a173089a6d1067fbf4ff859a38fb1e036d6c9
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-23 10:46:28 +0200

    kern: Extend platform intrinsics.
    Add inline definitions for interrupts functions and add functions
    encapsulating processor barrier instructions.
    
    * kern/include/kern/platform.h: Fix include styles.
      (enable_interrupts): Make function inline.
      (disable_interrupts): Make function inline.
      (dsb): New inline function.
      (isb): New inline function.
    * kern/platform.c: New file.
    * kern/CMakeLists.txt: Compile in platform.c.

--- kern/include/kern/platform.h
+++ kern/include/kern/platform.h
@@ -1,13 +1,14 @@
 #ifndef BRADYPOS_PLATFORM_H
 #define BRADYPOS_PLATFORM_H
 
-#include "hardware.h"
+#include <kern/hardware.h>
 #include <stdint.h>
 
 extern const uintptr_t __vector[];
 extern void _start();
 
-static inline void enable_interrupts() { __asm__ volatile("cpsie i"); }
-static inline void disable_interrupts() { __asm__ volatile("cpsid i"); }
-
+inline void enable_interrupts() { __asm__ volatile("cpsie i"); }
+inline void disable_interrupts() { __asm__ volatile("cpsid i"); }
+inline void dsb() { asm volatile("dsb" ::: "memory"); }
+inline void isb() { asm volatile("isb" ::: "memory"); }
 #endif
---
+++ kern/platform.c
@@ -0,0 +1,6 @@
+#include <kern/platform.h>
+
+void enable_interrupts();
+void disable_interrupts();
+void dsb();
+void isb();
