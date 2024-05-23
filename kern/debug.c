127c50fb08b6619a0a614f90a9216b2fc2433e00
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-23 14:08:55 +0200

    kern: Print faultign context on HardFault.
    * kern/debug.c (isr_hardfault): Pass original LR to __isr_hardfault.
      (__isr_hardfault): Print info about faulting context.

--- kern/debug.c
+++ kern/debug.c
@@ -1,5 +1,6 @@
 #include <kern/debug.h>
 #include <kern/interrupts.h>
+#include <kern/thread.h>
 #include <semihosting.h>
 #include <stdarg.h>
 #include <tinyprintf.h>
@@ -60,9 +61,46 @@ void panic(const char *fmt, ...)
     reset_processor();
 }
 
-static __attribute__((used)) void __isr_hardfault()
+static __attribute__((used)) void __isr_hardfault(unsigned lr)
 {
     dbg_log(DBG_INTERRUPT, "Executing HardFault\n");
+
+    bool fault_in_thread_mode = (lr & (1 << 3)) != 0;
+    bool uses_psp = (lr & (1 << 2)) != 0;
+    unsigned *faulted_sp = uses_psp ? get_psp() : get_msp();
+    dbg_printf(
+        "Context stored to %s\n"
+        "mode:\t%s\n"
+        "SP:\t0x%8x\n"
+        "PSR:\t0x%8x\n"
+        "r0:\t0x%8x\n"
+        "r1:\t0x%8x\n"
+        "r2:\t0x%8x\n"
+        "r3:\t0x%8x\n"
+        "r12:\t0x%8x\n"
+        "lr:\t0x%8x\n"
+        "pc:\t0x%8x\n",
+        uses_psp ? "PSP" : "MSP", fault_in_thread_mode ? "thread" : "handler",
+        (unsigned)faulted_sp, faulted_sp[THREAD_CTX_STACK_PSR],
+        faulted_sp[THREAD_CTX_STACK_R0], faulted_sp[THREAD_CTX_STACK_R1],
+        faulted_sp[THREAD_CTX_STACK_R2], faulted_sp[THREAD_CTX_STACK_R3],
+        faulted_sp[THREAD_CTX_STACK_R12], faulted_sp[THREAD_CTX_STACK_LR],
+        faulted_sp[THREAD_CTX_STACK_PC]);
+
     panic("!!! PANIC: Hardfault triggered\n");
 }
-DECLARE_ISR(isr_hardfault, __isr_hardfault)
+
+__attribute__((naked)) void isr_hardfault()
+{
+    asm("movs r0, r9\n\t"
+        "movs r1, lr\n\t"
+        "push {r0, lr}\n\t"
+        "ldr r0, =0x20040000\n\t"
+        "ldr r0, [r0]\n\t"
+        "movs r9, r0\n\t"
+        "movs r0, r1\n\t"
+        "bl __isr_hardfault\n\t"
+        "pop {r0}\n\t"
+        "movs r9, r0\n\t"
+        "pop {pc}\n\t");
+}
