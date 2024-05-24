f4a88b0772bf69004bbdebfcab74b20605329601
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-24 16:20:09 +0200

    * root/root.c (main): Fix error checkign.

--- root/root.c
+++ root/root.c
@@ -57,7 +57,7 @@ int main()
     {
         kill_root_thread();
     }
-    if (L4_set_priority(romfs_thread_id, 60) != L4_tstate_error)
+    if (L4_set_priority(romfs_thread_id, 60) == L4_tstate_error)
     {
         kill_root_thread();
     }
