4a2c623ea9f6d6b29540300cff761e99da7efe99
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-28 20:11:44 +0200

    l4: Add some rough implementations of L4_timeout functions.
    * lib/l4/schedule.c: Add a bunch of new, mostly untested functions.

--- lib/l4/schedule.c
+++ lib/l4/schedule.c
@@ -10,6 +10,62 @@ bool L4_is_clock_equal(L4_clock_t l, L4_clock_t r);
 bool L4_is_clock_not_equal(L4_clock_t l, L4_clock_t r);
 L4_clock_t L4_system_clock();
 
+L4_time_t L4_time_add_usec(L4_time_t l, uint32_t r)
+{
+    return (L4_time_t){.e = l.e, .m = l.m + (r >> l.e)};
+}
+
+L4_time_t *L4_time_add_usec_to(L4_time_t *l, uint32_t r)
+{
+    l->m += r >> l->e;
+    return l;
+}
+
+L4_time_t L4_time_sub_usec(L4_time_t l, uint32_t r)
+{
+    return (L4_time_t){.e = l.e, .m = l.m - (r >> l.e)};
+}
+
+L4_time_t *L4_time_sub_usec_from(L4_time_t *l, uint32_t r)
+{
+    l->m -= r >> l->e;
+    return l;
+}
+
+static inline unsigned max_unsigned(unsigned a, unsigned b)
+{
+    return (a < b ? b : a);
+}
+
+static unsigned adj_fix_value(L4_time_t v, unsigned e)
+{
+    return ((v.m << 22) >> (e - v.e));
+}
+
+bool L4_is_time_longer(L4_time_t l, L4_time_t r)
+{
+    const unsigned exp = max_unsigned(l.e, r.e);
+    return adj_fix_value(l, exp) > adj_fix_value(r, exp);
+}
+
+bool L4_is_time_shorter(L4_time_t l, L4_time_t r)
+{
+    const unsigned exp = max_unsigned(l.e, r.e);
+    return adj_fix_value(l, exp) < adj_fix_value(r, exp);
+}
+
+bool L4_is_time_equal(L4_time_t l, L4_time_t r)
+{
+    const unsigned exp = max_unsigned(l.e, r.e);
+    return adj_fix_value(l, exp) == adj_fix_value(r, exp);
+}
+
+bool L4_is_time_not_equal(L4_time_t l, L4_time_t r)
+{
+    const unsigned exp = max_unsigned(l.e, r.e);
+    return adj_fix_value(l, exp) != adj_fix_value(r, exp);
+}
+
 unsigned L4_schedule(L4_thread_id dest, unsigned time_control,
                      unsigned processor_control, unsigned prio,
                      unsigned preemption_control, unsigned *old_time_control);
