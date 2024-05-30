7f2d997dcc4d84792357c09e0cdada23db6f76f2
Tim Gesthuizen <tim@gesthuizen.net>
2024-05-30 16:24:36 +0200

    kern: Register correct UTCB location for root thread.
    IPC didn't work properly, as the kernel still had NULL stored for the
    UTCB location of the root thread. Fix this.
    
    * kern/systhread.c (create_sys_threads): Save location of the root
      thread UTCB.

--- kern/systhread.c
+++ kern/systhread.c
@@ -98,6 +98,7 @@ void create_sys_threads()
     root_tcb = create_thread(root_id);
     root_tcb->scheduler = kern_id;
     root_tcb->pager = kern_id;
+    root_tcb->utcb = root_utcb;
     unsigned *root_sp = (unsigned *)root_stack;
     root_sp -= 8;
     root_sp[THREAD_CTX_STACK_R0] = 0;
