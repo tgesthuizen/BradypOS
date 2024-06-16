eac98a242c0b96cf041e6282d170b689fa9e2099
Tim Gesthuizen <tim@gesthuizen.net>
2024-06-16 14:45:58 +0200

    kern: Properly handle hardfaults from user processes.
    * kern/debug.c (__isr_hardfault): Deleted.
      (isr_hardfault): Deleted.
    * kern/memory.c (__isr_hardfault): New function.
      (isr_hardfault): New function.

--- kern/debug.c
+++ kern/debug.c
@@ -1,6 +1,5 @@
 #include <kern/debug.h>
 #include <kern/interrupts.h>
-#include <kern/thread.h>
 #include <semihosting.h>
 #include <stdarg.h>
 #include <tinyprintf.h>
@@ -59,53 +58,4 @@ void panic(const char *fmt, ...)
     va_end(args);
 
     reset_processor();
-}
-
-static __attribute__((used)) void __isr_hardfault(unsigned lr)
-{
-    dbg_log(DBG_INTERRUPT, "Executing HardFault\n");
-
-    bool fault_in_thread_mode = (lr & (1 << 3)) != 0;
-    bool uses_psp = (lr & (1 << 2)) != 0;
-    unsigned *faulted_sp = uses_psp ? get_psp() : get_msp();
-    dbg_printf(
-        "Context stored to %s\n"
-        "mode:\t%s\n"
-        "SP:\t0x%08x\n"
-        "PSR:\t0x%08x\n"
-        "r0:\t0x%08x\n"
-        "r1:\t0x%08x\n"
-        "r2:\t0x%08x\n"
-        "r3:\t0x%08x\n"
-        "r12:\t0x%08x\n"
-        "lr:\t0x%08x\n"
-        "pc:\t0x%08x\n",
-        uses_psp ? "PSP" : "MSP", fault_in_thread_mode ? "thread" : "handler",
-        (unsigned)faulted_sp, faulted_sp[THREAD_CTX_STACK_PSR],
-        faulted_sp[THREAD_CTX_STACK_R0], faulted_sp[THREAD_CTX_STACK_R1],
-        faulted_sp[THREAD_CTX_STACK_R2], faulted_sp[THREAD_CTX_STACK_R3],
-        faulted_sp[THREAD_CTX_STACK_R12], faulted_sp[THREAD_CTX_STACK_LR],
-        faulted_sp[THREAD_CTX_STACK_PC]);
-
-    panic("!!! PANIC: Hardfault triggered\n");
-}
-
-__attribute__((naked)) void isr_hardfault()
-{
-    asm("movs r0, r9\n\t"
-        "movs r1, lr\n\t"
-        "push {r0, lr}\n\t"
-#ifndef NDEBUG
-        ".cfi_def_cfa_offset 8\n\t"
-        ".cfi_offset 9, -8\n\t"
-        ".cfi_offset 14, -4\n\t"
-#endif
-        "ldr r0, =0x20040000\n\t"
-        "ldr r0, [r0]\n\t"
-        "movs r9, r0\n\t"
-        "movs r0, r1\n\t"
-        "bl __isr_hardfault\n\t"
-        "pop {r0}\n\t"
-        "movs r9, r0\n\t"
-        "pop {pc}\n\t");
 }
