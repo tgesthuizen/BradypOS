4e4c223c8efe47647be19c510af500504b5237de
Tim Gesthuizen <tim@gesthuizen.net>
2024-06-24 14:25:30 +0200

    l4: Define term protocol.
    * include/term.h: New file.

---
+++ include/term.h
@@ -0,0 +1,49 @@
+#ifndef BRADYPOS_TERM_H
+#define BRADYPOS_TERM_H
+
+enum term_ops
+{
+    TERM_READ,
+    TERM_WRITE,
+};
+
+enum term_io_flags
+{
+    TERM_BLOCK,
+};
+
+enum term_read_args
+{
+    TERM_READ_OP,
+    TERM_READ_FLAGS,
+    TERM_READ_SIZE,
+};
+
+enum term_write_args
+{
+    TERM_WRITE_OP,
+    TERM_WRITE_FLAGS,
+    TERM_WRITE_SIZE,
+    TERM_WRITE_BUF,
+};
+
+enum term_answer_ops
+{
+    TERM_ERROR,
+    TERM_READ_RET,
+    TERM_WRITE_RET,
+};
+
+enum term_read_ret_args
+{
+    TERM_READ_RET_OP,
+    TERM_READ_RET_STR,
+};
+
+enum term_write_ret_args
+{
+    TERM_WRITE_RET_OP,
+    TERM_WRITE_RET_SIZE,
+};
+
+#endif
