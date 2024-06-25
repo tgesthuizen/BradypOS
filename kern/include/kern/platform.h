c3a346b7fba849dd223b84ff5550ad2e3728ab8b
Tim Gesthuizen <tim@gesthuizen.net>
2024-06-25 15:03:26 +0200

    kern: Mark enable/disable interrupts as clobbering memory.
    This shall force the compiler to treat the instructions with atomic
    semantics. Otherwise this can cause writes to be delayed incorrectly.
    
    * kern/inlcude/kern/platform.h (enable_interrupts): Mark as clobbering
      memory.
      (disable_interrupts): Mark as clobbering memory.

--- kern/include/kern/platform.h
+++ kern/include/kern/platform.h
@@ -7,8 +7,8 @@
 extern const uintptr_t __vector[];
 extern void _start();
 
-inline void enable_interrupts() { __asm__ volatile("cpsie i"); }
-inline void disable_interrupts() { __asm__ volatile("cpsid i"); }
+inline void enable_interrupts() { __asm__ volatile("cpsie i" ::: "memory"); }
+inline void disable_interrupts() { __asm__ volatile("cpsid i" ::: "memory"); }
 inline void dsb() { asm volatile("dsb" ::: "memory"); }
 inline void isb() { asm volatile("isb" ::: "memory"); }
 inline unsigned *get_msp()
