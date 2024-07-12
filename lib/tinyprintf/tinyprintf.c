81a834c1a6d6a1a8bf23358585f127089ee63c1d
Tim Gesthuizen <tim@gesthuizen.net>
2024-07-12 14:04:15 +0200

    tinyprintf: Fix bug in flag storage.
    * lib/tinyprintf/tinyprintf.c: Use unsigned type for bit flags.

--- lib/tinyprintf/tinyprintf.c
+++ lib/tinyprintf/tinyprintf.c
@@ -69,17 +69,18 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 /*
  * Implementation
  */
-struct param {
-    char lz:1;          /**<  Leading zeros */
-    char alt:1;         /**<  alternate form */
-    char uc:1;          /**<  Upper case (for base16 only) */
-    char align_left:1;  /**<  0 == align right (default), 1 == align left */
+struct param
+{
+    unsigned char lz : 1;  /**<  Leading zeros */
+    unsigned char alt : 1; /**<  alternate form */
+    unsigned char uc : 1;  /**<  Upper case (for base16 only) */
+    unsigned char
+        align_left : 1; /**<  0 == align right (default), 1 == align left */
     unsigned int width; /**<  field width */
     char sign;          /**<  The sign to display (if any) */
     unsigned int base;  /**<  number base (e.g.: 8, 10, 16) */
     char *bf;           /**<  Buffer to output */
 };
-
 
 #ifdef PRINTF_LONG_LONG_SUPPORT
 static void _TFP_GCC_NO_INLINE_ ulli2a(
