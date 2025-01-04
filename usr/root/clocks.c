663982f57ebf6feaf5ebe64634766d564d4e9254
Tim Gesthuizen <tim@gesthuizen.net>
2025-01-04 23:18:38 +0100

    root: Fix bit logic in PLL setup.
    Yet another incorrect bit logic to be fixed.
    
    * usr/root/clocks.c (setup_pll): Fix bit logic in check whether PLL
      has stabilized.

--- usr/root/clocks.c
+++ usr/root/clocks.c
@@ -102,7 +102,7 @@ void setup_pll(unsigned char *base, unsigned fbdiv, unsigned postdiv1,
     *(volatile unsigned *)(base + pll_pwr) &=
         ~((1 << pll_pwr_pd) | (1 << pll_pwr_vcopd));
     // Wait for the PLL to lock
-    while ((*(volatile unsigned *)(base + pll_cs) & pll_cs_lock) != 0)
+    while ((*(volatile unsigned *)(base + pll_cs) & (1 << pll_cs_lock)) != 0)
         ;
 
     // Setup post dividers
